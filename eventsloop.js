209ceeb2f7b3d1791aa135ce5c2ba0d87b3fb781
Mounika <bojjamounika8@gmail.com>
2022-01-05 15:30:09 +0530

    threadloop

---
+++ eventsloop.js
@@ -0,0 +1,36 @@
+// Inside the eventloop architecture program
+
+const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 4;
+
+setTimeout(() => console.log("Timer 1 finished"), 0);
+setImmediate(() => console.log("Immediate 1 finished"));
+
+fs.readFile("test-file.txt",() => {
+    console.log("I/O finished");
+    console.log("--------------");
+
+    setTimeout(() => console.log("Timer 2 finished"), 0);
+    setTimeout(() => console.log("Time 3 finished"), 5000);
+    setImmediate(() => console.log("Immediate 2 finished"));
+
+    process.nextTick(() => console.log("process.nextTick"));
+
+    crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+    console.log(Date.now() - start, "password encrypted");
+
+    crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+    console.log(Date.now() - start, "password encrypted");
+
+    crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+    console.log(Date.now() - start, "password encrypted");
+
+    crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+    console.log(Date.now() - start, "password encrypted");
+
+});
+
+console.log("Hello from the top-level code");
